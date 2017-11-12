115d17f68e07c2344d7e2d9cd4eb47a86e2e142c
rajib.bhowmik <rajib.bhowmik@gmail.com>
2017-11-12 21:21:19 +0100

    Add project files.

---
+++ Read_Write_StreamReader_StreamWriter/Demo.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Read_Write_StreamReader_StreamWriter
+{
+	class Demo
+	{
+		public static void Main(string[] args)
+		{
+			String filePath = "test.txt";
+
+			WriteToFile(filePath);
+			ReadFromFile(filePath);
+		}
+		public static void ReadFromFile(string filePath)
+		{
+			StreamReader reader = new StreamReader(filePath);
+			int count = 0;
+			while (!reader.EndOfStream)
+			{
+				Console.WriteLine(reader.ReadLine());
+				count++;
+			}
+
+			Console.WriteLine("There are {0} names in your text file", count);
+			reader.Close();
+		}
+
+		public static void WriteToFile(string filePath)
+		{
+			StreamWriter writer = new StreamWriter(filePath, true);
+			String temp = String.Empty;
+
+			do
+			{
+				Console.Write("Enter a name [-1 to exit]: ");
+				temp = Console.ReadLine();
+
+				if (temp != "-1")
+				{
+					writer.WriteLine(temp);
+				}
+
+			} while (temp != "-1");
+
+			writer.Close();
+		}
+	}
+}
+
+
